2556a9d8e20e7cc7fe9e3cffdf673c521ea8f89d
Loïc Herr <loic@herrloc.fr>
2018-11-29 22:43:49 +0100

    Home : article à la une

---
+++ front-page.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+<main role="main">
+
+    <article class="full-new">
+        <a href="#_">
+            <div class="news-wrapper">
+                <span class="date">18 janvier 2019</span>
+                <h2>Du Fansipan aux rizières de La Pan Tan</h2>
+                <button class="bt-arrow">
+                   <i></i>Lire l'article
+                </button>
+            </div>
+            <div class="img-wrapper">
+                <img src="https://picsum.photos/1920/1080" alt="">
+            </div>
+        </a>
+    </article>
+    
+    <article>
+        yo
+    </article>
+
+</main>
+
+<?php get_footer(); ?>
--- header.php
+++ header.php
@@ -1,37 +1,27 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
-		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title>
+        <?php wp_title(''); ?>
+        <?php if(wp_title('', false)) { echo ' :'; } ?>
+        <?php bloginfo('name'); ?>
+    </title>
 
-		<?php wp_head(); ?>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
 
-	</head>
-	<body <?php body_class(); ?>>
+    <?php wp_head(); ?>
 
-		<!-- wrapper -->
-		<div class="wrapper">
+</head>
 
-			<!-- header -->
-			<header class="header clear" role="banner">
+<body <?php body_class(); ?>>
 
-					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
-					</div>
-					<!-- /logo -->
+    <nav class="nav" role="navigation">
+        <?php wp_nav_menu(); ?>
+    </nav>
 
-					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
-					</nav>
-					<!-- /nav -->
-
-			</header>
-			<!-- /header -->
+    <!-- wrapper -->
+    <div class="wrapper">

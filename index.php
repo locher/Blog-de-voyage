c6845fc95c889723363e474797d6c0779559ecf6
Loïc Herr <loic@herrloc.fr>
2019-12-10 20:47:22 +0700

    Ajout pagination sur liste articles

--- index.php
+++ index.php
@@ -3,20 +3,33 @@
 
 <main role="main">
 
-    <?php
+	<?php
 		if ( have_posts() ) : ?>
-        <?php
+		<?php
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
     $size = 'h50w100';
         ?>
 
-            <article class="full-new w100 h50">
-                <?php get_template_part('template-parts/new-single'); ?>
-            </article>
+			<article class="full-new w100 h50">
+				<?php get_template_part('template-parts/new-single'); ?>
+			</article>
+
+
+
 
 </main>
 
 <?php endwhile; endif;?>
 
+<div class="pagination">
+			<?php 
+			$args_pagination = array(
+				'before_page_number' => ''
+			);
+	
+			echo get_the_posts_pagination($args_pagination);
+	
+		?>
+</div>
 <?php get_footer(); ?>

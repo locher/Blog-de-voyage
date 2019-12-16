ff51de4127ed87f5b8cc61ca632e6f93f171506f
Loïc Herr <loic@herrloc.fr>
2019-12-16 10:07:19 +0100

    Liste articles sur 2 colonnes

--- functions.php
+++ functions.php
@@ -58,7 +58,7 @@ function html5blank_conditional_scripts()
 // Load HTML5 Blank styles
 function html5blank_styles()
 {
-    wp_register_style('html5blank', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
+    wp_register_style('html5blank', get_template_directory_uri() . '/style.css', array(), '1.1', 'all');
     wp_enqueue_style('html5blank'); // Enqueue it!
     
     wp_register_style('fancycss', get_template_directory_uri() . '/js/fancybox/jquery.fancybox.min.css"', array(), '1.0', 'all');
--- index.php
+++ index.php
@@ -1,31 +1,32 @@
 <?php get_header(); ?>
 
 
-<main role="main">
+<main role="main" class="home-second">
 
 	<?php
 		if ( have_posts() ) : ?>
 		<?php
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
-    $size = 'h50w100';
+    $size = 'h50w50';
         ?>
 
-			<article class="full-new w100 h50">
+			<article class="full-new w50 h50">
 				<?php get_template_part('template-parts/new-single'); ?>
 			</article>
 
 
-
+<?php endwhile; endif;?>
 
 </main>
 
-<?php endwhile; endif;?>
+
 
 <div class="pagination">
 			<?php 
 			$args_pagination = array(
-				'before_page_number' => ''
+				'before_page_number' => '',
+				'prev_text' => '<i></i>'
 			);
 	
 			echo get_the_posts_pagination($args_pagination);
